ed1625d50bdbaa9eed9626cb889a53991621e5dc
jacobkosmart <jacobkosmart@gmail.com>
2022-03-07 10:11:50 +0900

    Initial Commit

---
+++ SwiftuiThinkingIntermediate/SwiftuiThinkingIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiThinkingIntermediateApp.swift
+//  SwiftuiThinkingIntermediate
+//
+//  Created by Jacob Ko on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiThinkingIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
